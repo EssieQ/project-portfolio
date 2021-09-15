1ac0fc387e674fe90bc287ca356efb4204056944
EssieQ <ecauven@gmail.com>
2021-09-15 18:41:51 +1000

    initial commit

---
+++ src/components/NavigationMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NavigationMenu() {
+  return (
+    <div className="NavigationMenu">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
