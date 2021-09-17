e534e3bb834eeb14066ca3ff2757620129e3a74e
EssieQ <ecauven@gmail.com>
2021-09-17 17:48:14 +1000

    Built navigation bar with router react dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import NavigationMenu from "./components/NavigationMenu";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <NavigationMenu />
+      <Router>
+        <NavigationMenu />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
