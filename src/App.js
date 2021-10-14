7f798890c36832eaa7eaed72891d5329b38ececd
EssieQ <ecauven@gmail.com>
2021-10-14 15:13:46 +1000

    Scroll to top

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavigationMenu />
+        <ScrollToTop />
         <Switch>
           <Route path="/about">
             <About />
